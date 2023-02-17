7b2b40743c045f27177bf46eeea8de67267d552f
joe swain <spunev1337@gmail.com>
2023-02-17 22:02:19 +0300

    clean

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import { fabric } from "fabric";
-import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 import './index.css'
 
 function App() {
-  const { editor, onReady } = useFabricJSEditor();
-  const onAddRectangle = () => {
-        editor.addRectangle();
-  };
-  const addNineRectangels = () => {
-    [...Array(9).keys()].forEach(() => {
-      onAddRectangle()
-    })
-    }
   return (
     <div className="App">
-     <button onClick={addNineRectangels}>123</button>
-     <FabricJSCanvas className="sample-canvas" onReady={onReady} />
-    </div>
+     </div>
   )
 }
 
