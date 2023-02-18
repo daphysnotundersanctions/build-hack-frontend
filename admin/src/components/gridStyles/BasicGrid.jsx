1f91239d6843b5eb297292b2c8fc9f2febe9d1b0
joe swain <spunev1337@gmail.com>
2023-02-18 21:51:38 +0300

    heeelp

--- admin/src/components/gridStyles/BasicGrid.jsx
+++ admin/src/components/gridStyles/BasicGrid.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Grid.css'
-import Sheet from '@mui/joy/Sheet';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/joy/Grid';
 import {supabase} from "../../API/API.js";
@@ -9,29 +8,23 @@ import {supabase} from "../../API/API.js";
 const squereSet = ["100","200","300","100","200","300","100","200","300",];
 
 const colorSet = {
-  freeNeedRepair : {
-    color : 'nvert(100%) sepia(24%) saturate(6028%) hue-rotate(317deg) brightness(91%) contrast(86%)'
-  },
-  freeNoRepair : {
-    color : 'invert(90%) sepia(5%) saturate(3652%) hue-rotate(59deg) brightness(90%) contrast(100%)'
-  },
-  busyOnRepair : {
-    color : 'invert(58%) sepia(12%) saturate(2484%) hue-rotate(333deg) brightness(107%) contrast(80%)'
-  },
-  busyNoRepair : {
-    color : 'invert(79%) sepia(13%) saturate(202%) hue-rotate(244deg) brightness(81%) contrast(90%)'
-  }
+    freeNeedRepair : {
+      color : 'invert(100%) sepia(24%) saturate(6028%) hue-rotate(317deg) brightness(91%) contrast(86%)'
+    },
+    freeNoRepair : {
+      color :  'invert(90%) sepia(5%) saturate(3652%) hue-rotate(59deg) brightness(90%) contrast(100%)'
+    },
+    busyOnRepair :{
+      color : 'invert(58%) sepia(12%) saturate(2484%) hue-rotate(333deg) brightness(107%) contrast(80%)'
+    },
+    busyNoRepair : {
+      color : 'invert(79%) sepia(13%) saturate(202%) hue-rotate(244deg) brightness(81%) contrast(90%)'
+    },
 }
 
-const Item = styled(Sheet)(({ theme }) => ({
-  ...theme.typography.body2,
-  color: theme.vars.palette.text.tertiary,
-}));
 
 
-
-
-const ShowContent = styled('div')(({size}) => ({
+const ShowContent = styled('div')(({size, stageName}) => ({
   '&:hover::before' : {
     content: `"${size}"`,
     position : 'absolute',
@@ -41,23 +34,26 @@ const ShowContent = styled('div')(({size}) => ({
   },
   '&:hover': {
     'img' : {
-      filter: 'blur(0.5px)',
+      // filter: colorSet.stageName.color,
     }
   }
 }));
 
 const imageSet = ['0','1','2_door1','0','1','2_door1','0','1','2_door1',]
-const drawImgs = imageSet.map((i,id) => 
-    <ShowContent key={i} size={squereSet[id]} sx={{maxWidth: 'fit-content', height: '150px'}}>
-      <Grid>
-        <img className='gridImg' width={'150'} src={`./blueprint/${i}.png`} />
-      </Grid>
-    </ShowContent>
-)
+
 export default function BasicGrid() {
   const [loading, setLoading] = React.useState(false);
   const [centers, setPlaces] = React.useState([]);
 
+
+  const drawImgs = imageSet.map((i,id) => 
+      <ShowContent key={id} size={squereSet[id]} stageName={centers[id]?.status} sx={{maxWidth: 'fit-content', height: '150px'}}>
+        <Grid>
+          <img className='gridImg' width={'150'} src={`./blueprint/${i}.png`} />
+        </Grid>
+      </ShowContent>
+  )
+  
   const getAllCenters = async () => {
     try {
         setLoading(true)
